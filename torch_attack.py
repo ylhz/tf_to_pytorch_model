74bf99c12cf08f1a966ebb539537a1cf3f55babd
ylhz <shengming@stu.sicau.edu.cn>
2022-11-11 19:55:10 +0800

    Change Name
    To avoid ambiguity, the prefix "tf_" has been changed to "tf2torch_".

--- torch_attack.py
+++ torch_attack.py
@@ -17,30 +17,30 @@ from tqdm import tqdm
 from PIL import Image
 
 from torch_nets import (
-    tf_inception_v3,
-    tf_inception_v4, 
-    tf_resnet_v2_50,
-    tf_resnet_v2_101,
-    tf_resnet_v2_152,
-    tf_inc_res_v2,
-    tf_adv_inception_v3,
-    tf_ens3_adv_inc_v3,
-    tf_ens4_adv_inc_v3,
-    tf_ens_adv_inc_res_v2,
+    tf2torch_inception_v3,
+    tf2torch_inception_v4, 
+    tf2torch_resnet_v2_50,
+    tf2torch_resnet_v2_101,
+    tf2torch_resnet_v2_152,
+    tf2torch_inc_res_v2,
+    tf2torch_adv_inception_v3,
+    tf2torch_ens3_adv_inc_v3,
+    tf2torch_ens4_adv_inc_v3,
+    tf2torch_ens_adv_inc_res_v2,
     )
 
 
 list_nets = [
-    'tf_inception_v3',
-    'tf_inception_v4',
-    'tf_resnet_v2_50',
-    'tf_resnet_v2_101',
-    'tf_resnet_v2_152',
-    'tf_inc_res_v2',
-    'tf_adv_inception_v3',
-    'tf_ens3_adv_inc_v3',
-    'tf_ens4_adv_inc_v3',
-    'tf_ens_adv_inc_res_v2'
+    'tf2torch_inception_v3',
+    'tf2torch_inception_v4',
+    'tf2torch_resnet_v2_50',
+    'tf2torch_resnet_v2_101',
+    'tf2torch_resnet_v2_152',
+    'tf2torch_inc_res_v2',
+    'tf2torch_adv_inception_v3',
+    'tf2torch_ens3_adv_inc_v3',
+    'tf2torch_ens4_adv_inc_v3',
+    'tf2torch_ens_adv_inc_res_v2'
     ]
 
 parser = argparse.ArgumentParser()
@@ -134,26 +134,26 @@ def get_model(net_name, model_dir):
     """Load converted model"""
     model_path = os.path.join(model_dir, net_name + '.npy')
 
-    if net_name == 'tf_inception_v3':
-        net = tf_inception_v3
-    elif net_name == 'tf_inception_v4':
-        net = tf_inception_v4
-    elif net_name == 'tf_resnet_v2_50':
-        net = tf_resnet_v2_50
-    elif net_name == 'tf_resnet_v2_101':
-        net = tf_resnet_v2_101
-    elif net_name == 'tf_resnet_v2_152':
-        net = tf_resnet_v2_152
-    elif net_name == 'tf_inc_res_v2':
-        net = tf_inc_res_v2
-    elif net_name == 'tf_adv_inception_v3':
-        net = tf_adv_inception_v3
-    elif net_name == 'tf_ens3_adv_inc_v3':
-        net = tf_ens3_adv_inc_v3
-    elif net_name == 'tf_ens4_adv_inc_v3':
-        net = tf_ens4_adv_inc_v3
-    elif net_name == 'tf_ens_adv_inc_res_v2':
-        net = tf_ens_adv_inc_res_v2
+    if net_name == 'tf2torch_inception_v3':
+        net = tf2torch_inception_v3
+    elif net_name == 'tf2torch_inception_v4':
+        net = tf2torch_inception_v4
+    elif net_name == 'tf2torch_resnet_v2_50':
+        net = tf2torch_resnet_v2_50
+    elif net_name == 'tf2torch_resnet_v2_101':
+        net = tf2torch_resnet_v2_101
+    elif net_name == 'tf2torch_resnet_v2_152':
+        net = tf2torch_resnet_v2_152
+    elif net_name == 'tf2torch_inc_res_v2':
+        net = tf2torch_inc_res_v2
+    elif net_name == 'tf2torch_adv_inception_v3':
+        net = tf2torch_adv_inception_v3
+    elif net_name == 'tf2torch_ens3_adv_inc_v3':
+        net = tf2torch_ens3_adv_inc_v3
+    elif net_name == 'tf2torch_ens4_adv_inc_v3':
+        net = tf2torch_ens4_adv_inc_v3
+    elif net_name == 'tf2torch_ens_adv_inc_res_v2':
+        net = tf2torch_ens_adv_inc_res_v2
     else:
         print('Wrong model name!')
 
@@ -242,7 +242,7 @@ def main():
         images = images.cuda()
 
         # Start Attack
-        adv_img = attack(models['tf_inception_v3'], images, label)
+        adv_img = attack(models['tf2torch_inception_v3'], images, label)
 
         # Save adversarial examples
         save_img(adv_img, filename, opt.output_dir)
